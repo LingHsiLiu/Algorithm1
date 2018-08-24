a01a5ca6424beaa557286e2f00a2e5996fc9bfeb
Althea <lhliu2@illinois.edu>
2018-08-24 12:19:47 -0500

    Create 140. Fast Power.py

---
+++ 2 - Binary Search - LogN Algorithm/140. Fast Power.py
@@ -0,0 +1,24 @@
+# Calculate the an % b where a, b and n are all 32bit integers.
+
+# Example
+# For 231 % 3 = 2
+
+# For 1001000 % 1000 = 0
+
+class Solution:
+    """
+    @param a: A 32bit integer
+    @param b: A 32bit integer
+    @param n: A 32bit integer
+    @return: An integer
+    """
+    def fastPower(self, a, b, n):
+        # write your code here
+        
+        ans = 1 
+        while n > 0:
+            if n % 2 == 1:
+                ans = ans * a % b
+            a = a * a % b
+            n = n // 2
+        return ans % b
