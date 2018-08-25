836446a3128f3564158d9de42cfe12ce0cfebaf8
Althea <lhliu2@illinois.edu>
2018-08-24 20:10:09 -0500

    Create 228. Middle of Linked List.py

---
+++ 3 - Two Pointers Algorithm/228. Middle of Linked List.py
@@ -0,0 +1,15 @@
+"""
+Definition of ListNode
+class ListNode(object):
+    def __init__(self, val, next=None):
+        self.val = val
+        self.next = next
+"""
+
+class Solution:
+    """
+    @param head: the head of linked list.
+    @return: a middle node of the linked list
+    """
+    def middleNode(self, head):
+        # write your code here
