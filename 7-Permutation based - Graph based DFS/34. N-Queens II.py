7fe3ba1689117192f55e3777c73bc1f107275382
Althea <lhliu2@illinois.edu>
2018-12-21 13:58:43 -0600

    Create 34. N-Queens II.py

---
+++ 7-Permutation based - Graph based DFS/34. N-Queens II.py
@@ -0,0 +1,40 @@
+# 34. N-Queens II
+# Follow up for N-Queens problem.
+
+# Now, instead outputting board configurations, return the total number of distinct solutions.
+
+# Example
+# For n=4, there are 2 distinct solutions.
+
+class Solution:
+    """
+    @param n: The number of queens.
+    @return: The total number of distinct solutions.
+    """
+    total = 0
+    n = 0
+
+    def attack(self, row, col):
+        for c, r in self.cols.items():
+            if c - r == col - row or c + r == col + row:
+                return True
+        return False
+
+    def search(self, row):
+        if row == self.n:
+            self.total += 1
+            return
+        for col in range(self.n):
+            if col in self.cols:
+                continue
+            if self.attack(row, col):
+                continue
+            self.cols[col] = row
+            self.search(row + 1)
+            del self.cols[col]
+
+    def totalNQueens(self, n):
+        self.n = n
+        self.cols = {}
+        self.search(0)
+        return self.total
