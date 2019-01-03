8c485f6c43145f16c25d424c4237386514415a42
Althea <lhliu2@illinois.edu>
2019-01-02 18:03:13 -0600

    Create 64. Merge Sorted Array.py

---
+++ 9-Data Structure-Interval-Array-Matrix-Binary Indexed Tree/64. Merge Sorted Array.py
@@ -0,0 +1,37 @@
+// 64. Merge Sorted Array
+// Given two sorted integer arrays A and B, merge B into A as one sorted array.
+
+// Example
+// A = [1, 2, 3, empty, empty], B = [4, 5]
+
+// After merge, A will be filled as [1, 2, 3, 4, 5]
+
+
+
+public class Solution {
+    /*
+     * @param A: sorted integer array A which has m elements, but size of A is m+n
+     * @param m: An integer
+     * @param B: sorted integer array B which has n elements
+     * @param n: An integer
+     * @return: nothing
+     */
+    public void mergeSortedArray(int[] A, int m, int[] B, int n) {
+        // write your code here
+        int i = m - 1, j = n - 1, index = m + n - 1;
+        while (i >= 0 && j >= 0){
+            if (A[i] > B[j]){
+                A[index--] = A[i--];
+            } else {
+                A[index--] = B[j--];
+                
+            }
+        }
+        while (i >= 0){
+            A[index--] = A[i--];
+        }
+        while (j >= 0){
+            A[index--] = B[j--];
+        }
+    }
+}
