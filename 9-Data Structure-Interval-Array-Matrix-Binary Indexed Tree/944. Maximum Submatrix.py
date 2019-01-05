b3d6d48877c1b5aa1dad6bfca6036be01214a309
Althea <lhliu2@illinois.edu>
2019-01-04 19:44:22 -0600

    Update 944. Maximum Submatrix.py

--- 9-Data Structure-Interval-Array-Matrix-Binary Indexed Tree/944. Maximum Submatrix.py
+++ 9-Data Structure-Interval-Array-Matrix-Binary Indexed Tree/944. Maximum Submatrix.py
@@ -27,7 +27,6 @@ class Solution:
             return 0
         if matrix[0] is None or len(matrix[0]) == 0:
             return 0
-        
         self.row = len(matrix)
         self.col = len(matrix[0])
         
@@ -37,37 +36,33 @@ class Solution:
         
         for up in range(self.row):
             for down in range(up, self.row):
-
                 arr = self.compression(matrix, up, down, prefix_sum)
                 max_sum = max(max_sum, self.maximumSubarray(arr))
         return max_sum
     
     def getPrefixSum(self, matrix):
-        sum = [[0 for i in range(self.col)] for j in range(self.row + 1)]
-        
+        sum_matrix = [[0 for i in range(self.col)] for j in range(self.row + 1)]
+
         for i in range(self.row):
             for j in range(self.col):
-                sum[i + 1][j] = sum[i][j] + matrix[i][j]
-
-        return sum
+                sum_matrix[i+1][j] += sum_matrix[i][j] + matrix[i][j]
+        return sum_matrix
     
     def compression(self, matrix, up, down, prefix_sum):
         arr = [0 for i in range(self.col)]
         for i in range(self.col):
             arr[i] = prefix_sum[down + 1][i] - prefix_sum[up][i]
-        
         return arr
         
     def maximumSubarray(self, arr):
         temp_max = -sys.maxint
         temp_min = 0
-        sum = 0
-        print(arr)
+        total = 0
         for i in range(len(arr)):
-            sum += arr[i]
-            if sum < temp_min:
-                temp_min = sum
-            if sum - temp_min > temp_max:
-                temp_max = sum - temp_min
-
+            total += arr[i]
+            if total < temp_min:
+                temp_min = total
+            if total - temp_min > temp_max:
+                temp_max = total - temp_min
         return temp_max
+        
